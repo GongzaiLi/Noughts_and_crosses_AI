59c0ca2c0ca5801a7e10ce590a65212f24b77e97
Gongzai Li <gli65@uclive.ac.nz>
2021-10-14 17:44:04 +1300

    done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from student_answer import optimal_move as student_optimal
+from student_answer import server_optimal
 
 
 def board_from_string(board_str):
@@ -19,23 +20,29 @@ class Terminal:
 
     def is_row_has_win(self):
         for row in self.board:
-            if row.count(row[0]) == len(row):
+            if row.count(row[0]) == len(row) and row[0] != '.':
                 self.winner_player = row[0]
                 return True
         return False
 
     def is_col_has_win(self):
         for col_i, col in enumerate(self.board[0]):
-            if all([col == row[col_i] for row in self.board]):
+            if all([col == row[col_i] and col != '.' for row in self.board]):
                 self.winner_player = col
                 return True
         return False
 
     def is_cross_has_win(self):
+        """
+        todo error
+        """
         left = self.board[0][0]
         right = self.board[0][-1]
-        left_check = all([left == self.board[i][i] for i in range(1, len(self.board))])
-        right_check = all([left == self.board[i][i] for i in range(len(self.board), 1, -1)])
+        left_check = all([left == self.board[i][i] and left != '.' for i in range(1, len(self.board))])
+
+        right_check = all([right == self.board[i][len(self.board) - 1 - i] and right != '.' for i in
+                           range(1, len(self.board))])  # range(start, end) end not call
+
         if left_check:
             self.winner_player = left
             return True
@@ -48,6 +55,7 @@ class Terminal:
         return all([col != '.' for row in self.board for col in row])
 
     def is_terminal(self):
+
         if self.is_row_has_win() or self.is_col_has_win() or self.is_cross_has_win():
             return True
         if self.game_over():
@@ -73,7 +81,6 @@ if __name__ == '__main__':
 
     board = board_from_string(board_str)
     player = student = 'X'
-    print(board)
     display(board)
 
     while not Terminal(board).is_terminal():
